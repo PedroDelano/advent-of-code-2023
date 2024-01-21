7335bb6a9f6ee2bc335b52e27d34ab88e709ac0c
Pedro Cavalcanti <pedelano@gmail.com>
2024-01-21 09:18:16 -0300

    style: google style formatting

--- day_2/main.java
+++ day_2/main.java
@@ -1,36 +1,36 @@
-// The Elf would first like to know which games would have been possible 
+// The Elf would first like to know which games would have been possible
 // if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
 import java.io.*;
 import java.util.HashMap;
-import java.util.regex.Pattern;
 import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 
 public class Main {
 
-  public static String trim(String st){
-    return st.replaceAll("\\s+","");
+  public static String trim(String st) {
+    return st.replaceAll("\\s+", "");
   }
 
-  public static int toInt(String st){
+  public static int toInt(String st) {
     st = trim(st);
     return Integer.parseInt(st);
   }
 
-  public static boolean isColor(String st){
+  public static boolean isColor(String st) {
     st = trim(st);
-    if (st.equals("red") || st.equals("blue") || st.equals("green")){
+    if (st.equals("red") || st.equals("blue") || st.equals("green")) {
       return true;
     }
     return false;
   }
 
-  public static HashMap<String, Integer> getSet(String set){
+  public static HashMap<String, Integer> getSet(String set) {
     HashMap<String, Integer> game = new HashMap<String, Integer>();
     game.put("red", 0);
     game.put("green", 0);
     game.put("blue", 0);
     String[] arrOfString = set.split(",", 0);
-    for (String elem: arrOfString){
+    for (String elem : arrOfString) {
       String color = elem.replaceAll("\\d+", "").replaceAll("\\s+", "");
       int numberOfBalls = toInt(elem.replaceAll("[a-zA-Z]", "").replaceAll("\\s+", ""));
       game.put(color, game.get(color) + numberOfBalls);
@@ -38,49 +38,50 @@ public class Main {
     return game;
   }
 
-  public static boolean isGameValid(String row){
+  public static boolean isGameValid(String row) {
     row = row.replaceAll("Game (\\d*):", "");
     String[] sets = row.split(";");
     // for each game set, checks to see if it's valid
     // considering the number of colored balls
-    for (String setString: sets){
+    for (String setString : sets) {
       HashMap<String, Integer> set = getSet(setString);
-      if (!isSetValid(set)){
+      if (!isSetValid(set)) {
         return false;
       }
     }
     return true;
   }
 
-  public static int getGameId(String row){
-    String[] matches = Pattern.compile("Game (\\d*):")
-      .matcher(row)
-      .results()
-      .map(MatchResult::group)
-      .toArray(String[]::new);
+  public static int getGameId(String row) {
+    String[] matches =
+        Pattern.compile("Game (\\d*):")
+            .matcher(row)
+            .results()
+            .map(MatchResult::group)
+            .toArray(String[]::new);
     String gameId = matches[0];
-    gameId = gameId.replaceAll("Game ","");
-    gameId = gameId.replaceAll(":","");
+    gameId = gameId.replaceAll("Game ", "");
+    gameId = gameId.replaceAll(":", "");
     return toInt(gameId);
   }
 
-  public static boolean isSetValid(HashMap<String, Integer> set){
-    if (set.get("red") > 12 || set.get("green") > 13 || set.get("blue") > 14){
+  public static boolean isSetValid(HashMap<String, Integer> set) {
+    if (set.get("red") > 12 || set.get("green") > 13 || set.get("blue") > 14) {
       return false;
     }
     return true;
   }
 
   public static void main(String[] args) throws Exception {
-      File file = new File("day_2/input.txt");
-      BufferedReader br = new BufferedReader(new FileReader(file));
-      String row;
-      int idSumOfValidGames = 0;
-      while ((row = br.readLine()) != null) {
-        if (isGameValid(row)){
-          idSumOfValidGames += getGameId(row);
-        }
+    File file = new File("day_2/input.txt");
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    String row;
+    int idSumOfValidGames = 0;
+    while ((row = br.readLine()) != null) {
+      if (isGameValid(row)) {
+        idSumOfValidGames += getGameId(row);
       }
-      System.out.println("Soma dos IDs de jogos válidos: " + idSumOfValidGames);
+    }
+    System.out.println("Soma dos IDs de jogos válidos: " + idSumOfValidGames);
   }
 }
--- day_3/main.java
+++ day_3/main.java
@@ -1,11 +1,10 @@
-// There are lots of numbers and symbols you don't really understand, 
-// but apparently any number adjacent to a symbol, even diagonally, 
+// There are lots of numbers and symbols you don't really understand,
+// but apparently any number adjacent to a symbol, even diagonally,
 // is a "part number" and should be included in your sum.
 import java.io.*;
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class Main {
 
@@ -17,7 +16,7 @@ public class Main {
     Integer ncol = 0;
     while ((row = br.readLine()) != null) {
       nrow += 1;
-      if (nrow == 1){
+      if (nrow == 1) {
         ncol = row.length();
       }
     }
@@ -26,16 +25,16 @@ public class Main {
     return matrixShape;
   }
 
-  public static char[][] constructInputMatrix(String row, int rowIndex, char[][] inputMatrix){
+  public static char[][] constructInputMatrix(String row, int rowIndex, char[][] inputMatrix) {
     // iterativelly constructs the input matrx
-    for (int colIndex = 0; colIndex < row.length(); colIndex++){
+    for (int colIndex = 0; colIndex < row.length(); colIndex++) {
       inputMatrix[rowIndex][colIndex] = row.charAt(colIndex);
     }
     return inputMatrix;
   }
 
   public static boolean isDigit(char c) {
-    String pattern = "[\\d]"; 
+    String pattern = "[\\d]";
     return Pattern.matches(pattern, String.valueOf(c));
   }
 
@@ -44,84 +43,84 @@ public class Main {
   }
 
   public static boolean isAdjacent(
-    ArrayList<Integer> numberIndex, 
-    char[][] stringMatrix, 
-    int rowIndex,
-    int nrow,
-    int ncol
-  ){
+      ArrayList<Integer> numberIndex, char[][] stringMatrix, int rowIndex, int nrow, int ncol) {
     // check if the element on the left is a valid symbol
-    if (numberIndex.get(0) > 0){
+    if (numberIndex.get(0) > 0) {
       if (isSymbol(stringMatrix[rowIndex][numberIndex.get(0) - 1])) return true;
     }
     // check if the element on the right is a valid symbol
     if (numberIndex.get(numberIndex.size() - 1) < ncol - 1) {
-      if (isSymbol(stringMatrix[rowIndex][numberIndex.get(numberIndex.size() - 1) + 1])) return true;
+      if (isSymbol(stringMatrix[rowIndex][numberIndex.get(numberIndex.size() - 1) + 1]))
+        return true;
     }
     // check if there's any symbol in the up and down
-    for (int index: numberIndex){
+    for (int index : numberIndex) {
       // checks up
       if (rowIndex > 0) {
         if (isSymbol(stringMatrix[rowIndex - 1][index])) return true; // checks up
-        if (index + 1 < nrow - 1) if (isSymbol(stringMatrix[rowIndex - 1][index + 1])) return true; // check upper right
-        if (index > 0) if (isSymbol(stringMatrix[rowIndex - 1][index - 1])) return true; // check upper left
+        if (index + 1 < nrow - 1)
+          if (isSymbol(stringMatrix[rowIndex - 1][index + 1])) return true; // check upper right
+        if (index > 0)
+          if (isSymbol(stringMatrix[rowIndex - 1][index - 1])) return true; // check upper left
       }
       // checks down
-      if (rowIndex < nrow - 1){
+      if (rowIndex < nrow - 1) {
         if (isSymbol(stringMatrix[rowIndex + 1][index])) return true; // checks down
-        if (index + 1 < nrow - 1) if (isSymbol(stringMatrix[rowIndex + 1][index + 1])) return true; // checks lower right
-        if (index > 0) if (isSymbol(stringMatrix[rowIndex + 1][index - 1])) return true; // checks lower left
+        if (index + 1 < nrow - 1)
+          if (isSymbol(stringMatrix[rowIndex + 1][index + 1])) return true; // checks lower right
+        if (index > 0)
+          if (isSymbol(stringMatrix[rowIndex + 1][index - 1])) return true; // checks lower left
       }
     }
     return false;
   }
 
   public static void main(String[] args) throws Exception {
-      String filePath = "day_3/input.txt";
-      File file = new File(filePath);
+    String filePath = "day_3/input.txt";
+    File file = new File(filePath);
 
-      // Get the dimensions for the matrix
-      BufferedReader br = new BufferedReader(new FileReader(file));
-      HashMap<String, Integer> matrixShape = getMatrixShape(br);
-      int nrow = matrixShape.get("nrow");
-      int ncol = matrixShape.get("ncol");
+    // Get the dimensions for the matrix
+    BufferedReader br = new BufferedReader(new FileReader(file));
+    HashMap<String, Integer> matrixShape = getMatrixShape(br);
+    int nrow = matrixShape.get("nrow");
+    int ncol = matrixShape.get("ncol");
 
-      // empty matrix template
-      char[][] stringMatrix = new char[nrow][ncol];
+    // empty matrix template
+    char[][] stringMatrix = new char[nrow][ncol];
 
-      // generate a matrix for the input
-      br = new BufferedReader(new FileReader(file));
-      String row;
-      int rowIndex = 0;
-      while ((row = br.readLine()) != null) {
-        stringMatrix = constructInputMatrix(row, rowIndex, stringMatrix);
-        rowIndex += 1;
-      }
+    // generate a matrix for the input
+    br = new BufferedReader(new FileReader(file));
+    String row;
+    int rowIndex = 0;
+    while ((row = br.readLine()) != null) {
+      stringMatrix = constructInputMatrix(row, rowIndex, stringMatrix);
+      rowIndex += 1;
+    }
 
-      int sumOfValidNumber = 0;
+    int sumOfValidNumber = 0;
 
-      // identify numbers and get the indexes
-      for (int i = 0; i < nrow; i++){
-        for (int j = 0; j < ncol; j++){
-          if (!isDigit(stringMatrix[i][j])) continue;
-          // if it finds a number will iterate until it reaches a dot again
-          ArrayList<Integer> numberIndex = new ArrayList<>();
-          for (int k = j; k < ncol; k++){
-            if (!isDigit(stringMatrix[i][j])) break;
-            numberIndex.add(k);
-            j++;
+    // identify numbers and get the indexes
+    for (int i = 0; i < nrow; i++) {
+      for (int j = 0; j < ncol; j++) {
+        if (!isDigit(stringMatrix[i][j])) continue;
+        // if it finds a number will iterate until it reaches a dot again
+        ArrayList<Integer> numberIndex = new ArrayList<>();
+        for (int k = j; k < ncol; k++) {
+          if (!isDigit(stringMatrix[i][j])) break;
+          numberIndex.add(k);
+          j++;
+        }
+        boolean isValid = isAdjacent(numberIndex, stringMatrix, i, nrow, ncol);
+        if (isValid) {
+          // join the numbers as a string and converts it into a Integer
+          String validNumber = "";
+          for (int elem : numberIndex) {
+            validNumber += stringMatrix[i][elem];
           }
-          boolean isValid = isAdjacent(numberIndex, stringMatrix, i, nrow, ncol);
-          if (isValid) {
-            // join the numbers as a string and converts it into a Integer
-            String validNumber = "";
-            for (int elem: numberIndex) {
-              validNumber += stringMatrix[i][elem];
-            }
-            sumOfValidNumber += Integer.parseInt(validNumber);
-          }
+          sumOfValidNumber += Integer.parseInt(validNumber);
         }
       }
+    }
     System.out.println("Sum of all valid numbers: " + sumOfValidNumber);
   }
 }
